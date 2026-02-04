a63eaa31fda68da1b3080601caea1e0b85d3c847
akmalputrautomo <apu.utomo@gmail.com>
2026-02-05 04:18:17 +0700

    fix

--- src/assets/pages/Request.jsx
+++ src/assets/pages/Request.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const Request = () => {
   const navigate = useNavigate();
 
